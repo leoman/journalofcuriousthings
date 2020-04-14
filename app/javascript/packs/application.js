19021246bf55310aab4169d6ac95e54efd06e68e
Peter Mansell <peter@pmansell.com>
2020-04-14 12:06:04 +0100

    - update frontend styling - remove old unwanted styles - update classes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("packs/grid")
 
+import '../../assets/styles/index.scss'
+      
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
