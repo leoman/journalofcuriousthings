c2ad70fb0404465f7846a62470d7717854d7bd9f
Peter Mansell <peter@pmansell.com>
2020-04-22 17:48:57 +0100

    - shrinking the header when you scroll so you can see more of the page

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -7,7 +7,22 @@ import SelectPure from "select-pure";
 window.SelectPure = SelectPure;
 
 import '../../assets/styles/admin.scss'
-   
+
 const componentRequireContext = require.context("components", true);
 const ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
+const resizeHeader = () => {
+
+  const html = document.getElementsByTagName('html')[0]
+  const header = document.getElementsByTagName('header')[0]
+  
+  if (html.scrollTop > 100) {
+    header.classList.add("shrink");
+  }
+  else {
+    header.classList.remove("shrink");
+  }
+}
+
+window.addEventListener("scroll", resizeHeader);
