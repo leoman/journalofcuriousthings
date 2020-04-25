9e36da80ad8778b7675fdee36e9c5a653fab5bf9
Peter Mansell <peter@pmansell.com>
2020-04-25 23:31:34 +0100

    - removing unwanted image size

--- app/javascript/components/SlateEditor/renderers/index.js
+++ app/javascript/components/SlateEditor/renderers/index.js
@@ -46,8 +46,6 @@ export const ImageElement = ({ attributes, children, element }) => {
             src={element.url}
             className={css`
               display: block;
-              max-width: 100%;
-              max-height: 20em;
               box-shadow: ${selected && focused ? '0 0 0 3px #B4D5FF' : 'none'};
             `}
           />
