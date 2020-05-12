03185c8251e72283d86787a8da3de3b1b38fff5b
Peter Mansell <peter@pmansell.com>
2020-05-12 18:40:32 +0100

    - update order to include new fields - adding field validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,14 +3,23 @@ class Order < ApplicationRecord
   enum payment_gateway: { paypal: 0 }
   belongs_to :product
 
-  scope :recently_created, ->  { where(created_at: 1.minutes.ago..DateTime.now) }
+  scope :recently_created, ->  { where(created_at: 10.minutes.ago..DateTime.now) }
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 120 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+  def set_paypal_payment_gateway
+    self.payment_gateway = Order.payment_gateways[:paypal]
+  end
 
   def set_paid
     self.status = Order.statuses[:paid]
   end
+
   def set_failed
     self.status = Order.statuses[:failed]
   end
+  
   def set_paypal_executed
     self.status = Order.statuses[:paypal_executed]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_110642) do
+ActiveRecord::Schema.define(version: 2020_05_11_161419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_05_04_110642) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "name"
+    t.string "email"
   end
 
   create_table "posts", force: :cascade do |t|
